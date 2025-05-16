9e1ddd47389b2a28f2ecb094ddeaaded61553afc
joseverdev <jmu3612@gmail.com>
2025-05-15 19:31:20 -0500

    feat(leetcode-1):two sum

---
+++ src/leetcode/1-two-sum.js
@@ -0,0 +1,81 @@
+/*PROBLEM
+1. Two Sum
+Easy
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+//SOLUTION
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums, target) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      const res = nums[i] + nums[j];
+      if (res === target) {
+        result.push(i, j);
+
+        break;
+      }
+    }
+    if (result.length > 0) {
+      break;
+    }
+  }
+  return result;
+};
+
+//Nota: esta fue mi solucion inicial, pero no es la mejor. Pues al hacer un bucle dentro de otro bucle, la complejidad es O(n^2).
+//Un mejor algoritmo es el siguiente:
+
+var twoSum2 = function (nums, target) {
+  const numToIndex = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const complement = target - num;
+
+    if (complement in numToIndex) {
+      return [numToIndex[complement], i];
+    }
+
+    numToIndex[num] = i;
+  }
+
+  return null;
+};
