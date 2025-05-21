6b68a3e76b6de48c365002bc98e2f467bc18f513
joseverdev <jmu3612@gmail.com>
2025-05-21 13:54:23 -0500

    feat(leetcode-13): add roman to integer fast solution

--- src/leetcode/13-roman-to-integer.js
+++ src/leetcode/13-roman-to-integer.js
@@ -67,3 +67,35 @@ var romanToInt = function (s) {
 console.log(romanToInt('IV'));
 console.log(romanToInt('IX'));
 console.log(romanToInt('X'));
+
+//segun leetcode esta solucion es mas rapida
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt2 = function (s) {
+  const romanValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+  let prevValue = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const currentValue = romanValues[s[i]];
+    if (currentValue < prevValue) {
+      total -= currentValue;
+    } else {
+      total += currentValue;
+    }
+    prevValue = currentValue;
+  }
+
+  return total;
+};
